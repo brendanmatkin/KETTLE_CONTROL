a95b513c3c5a5d25deb25300770a36cf2dad458f
Brendan <brendan@brendanmatkin.info>
2016-10-02 15:19:32 -0600

    cleanup! Remove old versions, change some names

--- marioTone/marioTone.ino
+++ marioTone/marioTone.ino
@@ -1,47 +1,52 @@
-unsigned long timer;
+unsigned long toneTimer;
 int tonePin = 6;
 int freq = 2637;
 
 void setup() {
   pinMode(tonePin, OUTPUT);
-  timer = millis();
+  toneTimer = millis();
   noTone(tonePin);
 
 }
 
 
-int sT = 3000; // start time
-int tL = 100;   // tone length
-int nL = 200;  // note length
+
 void loop() {
-  if (millis() - timer >= sT+2800+tL) {//2850
+  marioTone(150, 100);
+}
+
+void marioTone(int nL, int tL) {
+  int sT = 3000; // start time
+  //int tL = 75;   // tone length
+  //int nL = 120;  // note length
+  if (millis() - toneTimer >= sT + (nL * 12) + tL) { //2850
     noTone(tonePin);
-    timer = millis();
-  } else if (millis() - timer >= sT+2800) {//2800
-    tone(tonePin, freq-1050);                // 7
-  } else if (millis() - timer >= sT+1850+tL) {//1900
+    toneTimer = millis();
+  } else if (millis() - toneTimer >= sT + (nL * 12)) { //2800
+    tone(tonePin, freq - 1050);              // 7
+  } else if (millis() - toneTimer >= sT + (nL * 8) + tL) { //1900
     noTone(tonePin);
-  } else if (millis() - timer >= sT+1850) {//1850
-    tone(tonePin, freq+499);                // 6
-  } else if (millis() - timer >= sT+1400+tL) {//1450
+  } else if (millis() - toneTimer >= sT + (nL * 8)) { //1850
+    tone(tonePin, freq + 499);              // 6
+  } else if (millis() - toneTimer >= sT + (nL * 6) + tL) { //1450
     noTone(tonePin);
-  } else if (millis() - timer >= sT+1400) {//1400
+  } else if (millis() - toneTimer >= sT + (nL * 6)) { //1400
     tone(tonePin, freq);                     // 5
-  } else if (millis() - timer >= sT+1150+tL) {//1200
+  } else if (millis() - toneTimer >= sT + (nL * 5) + tL) { //1200
     noTone(tonePin);
-  } else if (millis() - timer >= sT+1150) {//1150
-    tone(tonePin, freq-544);                 // 4
-  } else if (millis() - timer >= sT+700+tL) {//750
+  } else if (millis() - toneTimer >= sT + (nL * 5)) { //1150
+    tone(tonePin, freq - 544);               // 4
+  } else if (millis() - toneTimer >= sT + (nL * 3) + tL) { //750
     noTone(tonePin);
-  } else if (millis() - timer >= sT+700) {//700
+  } else if (millis() - toneTimer >= sT + (nL * 3)) { //700
     tone(tonePin, freq);                     // 3
-  } else if (millis() - timer >= sT+250+tL) {//300
+  } else if (millis() - toneTimer >= sT + nL + tL) { //300
     noTone(tonePin);
-  } else if (millis() - timer >= sT+250) {//250
+  } else if (millis() - toneTimer >= sT + nL) { //250
     tone(tonePin, freq);                     // 2
-  } else if (millis() - timer >= sT+tL) {//50
+  } else if (millis() - toneTimer >= sT + tL) { //50
     noTone(tonePin);
-  } else if (millis() - timer >= sT) {
+  } else if (millis() - toneTimer >= sT) {
     tone(tonePin, freq);                      // 1
   } else {
     noTone(tonePin);
